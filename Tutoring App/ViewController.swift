d99d2052be538b72c60a36990b47e7cddd9dac1d
shubhambatra3019 <31996668+shubhambatra3019@users.noreply.github.com>
2018-03-03 14:38:20 -0500

    General UI for the app

--- Tutoring App/ViewController.swift
+++ Tutoring App/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func tutorButton(_ sender: Any) {
+    
+    }
+    
+    @IBAction func findATutor(_ sender: Any) {
+    
+    }
+    
 }
 
