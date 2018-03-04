c288be1bae3461d4858e244e3e0a1362f7f209fa
shubhambatra3019 <31996668+shubhambatra3019@users.noreply.github.com>
2018-03-04 11:38:33 -0500

    Added Login Page

---
+++ Tutoring App/ViewControllers/LoginPageViewController.swift
@@ -0,0 +1,51 @@
+//
+//  LoginPageViewController.swift
+//  Tutoring App
+//
+//  Created by Shubham Batra on 04/03/18.
+//  Copyright © 2018 Esper. All rights reserved.
+//
+
+import UIKit
+
+class LoginPageViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+   
+    
+    //func textFieldDidBeginEditing(_textField: UITextField) {
+        
+      //  saveButton.isEnabled = false
+        
+    //}
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
